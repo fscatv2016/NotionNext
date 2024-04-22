f0189b3edc71aa8a8cd82e55fdbed39ab9605911
fscatv2016 <131167169+fscatv2016@users.noreply.github.com>
2024-04-22 15:54:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -35,11 +35,8 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
     '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🤖️ 数码科技爱好者'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
